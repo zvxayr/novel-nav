b26a425614a1008cbf81804585d5c4414192af2b
zvxayr <carlo4noles@gmail.com>
2023-01-30 09:55:40 +0800

    -feat moved small functions to utils

--- src/renderer/components/DarkModeToggle.tsx
+++ src/renderer/components/DarkModeToggle.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
+import { pickProp, switchValues } from '../../utils';
+
 export const DarkModeToggle = () => {
     const [isDarkMode, setDarkMode] = useState<boolean | null>(null);
 
@@ -25,31 +27,17 @@ export const DarkModeToggle = () => {
     );
 };
 
-const switchValues =
-    (whenNull: string, whenTrue: string, whenFalse: string) =>
-    (switchValue: boolean | null): string => {
-        switch (switchValue) {
-            case true:
-                return whenTrue;
-            case false:
-                return whenFalse;
-            default:
-                return whenNull;
-        }
-    };
-
 const TogglerWrapper = styled.div<{ isDarkMode: boolean }>`
     border-radius: 20px;
     display: grid;
     height: 20px;
     width: 40px;
-    background-color: ${({ isDarkMode }) => switchValues('#555', '#333', '#CCC')(isDarkMode)};
-    grid-template-columns: ${({ isDarkMode }) =>
-        switchValues('0.5fr auto 0.5fr', '1fr auto 0fr', '0fr auto 1fr')(isDarkMode)};
+    background-color: ${pickProp('isDarkMode', switchValues('#333', '#CCC', '#555'))};
+    grid-template-columns: ${pickProp('isDarkMode', switchValues('1fr auto 0fr', '0fr auto 1fr', '0.5fr auto 0.5fr'))};
     transition: all 0.3s ease-in-out;
 
     &:hover :nth-child(2) {
-        background-color: ${({ isDarkMode }) => switchValues('#888', '#AAA', '#666')(isDarkMode)};
+        background-color: ${pickProp('isDarkMode', switchValues('#888', '#AAA', '#666'))};
     }
 `;
 
